4415dcf4875e6c3b075a3b79e888f3de1df96d6c
Max Gonzih <gonzih@gmail.com>
2015-01-02 21:46:25 +0100

    Now it's fully interactive

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject hello-atom-shell "0.1.0-SNAPSHOT"
   :source-paths ["src/tools"]
   :description "A hello, world application for atom-shell"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.webjars/react "0.12.0"]
+                 [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2511"]
                  [figwheel "0.1.5-SNAPSHOT"]
-                 ; [ring/ring-core "1.3.1"]
-                 ]
+                 [reagent "0.5.0-alpha"]
+                 [ring/ring-core "1.3.1"]]
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-figwheel "0.1.5-SNAPSHOT"]]
   :cljsbuild
@@ -22,8 +23,8 @@
                 :output-to "resources/public/js/ui-core.js",
                 :optimizations :none
                 ; :pretty-print true
-                ; :source-map true
+                :source-map true
                 :cache-analysis true}}]}
   :figwheel {:http-server-root "public"
-             ; :ring-handler src.tools.figwheel-ring-handler/app
+             :ring-handler figwheel-middleware/app
              :server-port 3449})
--- src/ui/core.cljs
+++ src/ui/core.cljs
@@ -1,10 +1,24 @@
 (ns ui.core
-  (:require [figwheel.client :as fw :include-macros true]))
+  (:require [figwheel.client :as fw :include-macros true]
+            [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
-
-(js/alert "Hello from cljs")
 
 (fw/watch-and-reload
   :websocket-url   "ws://localhost:3449/figwheel-ws"
   :jsload-callback (fn [] (print "reloaded")))
+
+(def state (atom 0))
+
+(defn root-component []
+  [:div
+   [:h1 "Hello world"]
+   [:button
+    {:on-click #(swap! state inc)}
+    (str "You clicked me "
+         @state
+         " times")]])
+
+(reagent/render
+  [root-component]
+  (.-body js/document))
