601b98b3bec890b508bdd2cb32f1c3ededdbd86f
Max Gonzih <gonzih@gmail.com>
2015-11-10 21:57:44 +0100

    Bump reagent version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [figwheel "0.5.0-SNAPSHOT"]
-                 [reagent "0.5.0"]
+                 [reagent "0.5.1"]
                  [ring/ring-core "1.4.0"]]
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-figwheel "0.5.0-SNAPSHOT"]]
