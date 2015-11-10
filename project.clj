ead5a9002f7922c234944c5a53036fd275f03af3
Max Gonzih <gonzih@gmail.com>
2015-11-10 21:28:08 +0100

    Add extra compilation targets

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hello-electron "0.1.0-SNAPSHOT"
   :source-paths ["src/tools"]
-  :description "A hello, world application for electron"
+  :description "A hello world application for electron"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.28"]
                  [figwheel "0.3.7"]
@@ -10,19 +10,32 @@
             [lein-figwheel "0.3.7"]]
   :cljsbuild
   {:builds
-   [{:source-paths ["src/electron"],
-     :id "electron-dev",
-     :compiler {:output-to "resources/main.js",
+   [{:source-paths ["src/electron"]
+     :id "electron-dev"
+     :compiler {:output-to "resources/main.js"
                 :optimizations :simple
                 :pretty-print true
                 :cache-analysis true}}
-    {:source-paths ["src/ui"],
-     :id "frontend-dev",
-     :compiler {:output-dir "resources/public/js/ui-out"
-                :output-to "resources/public/js/ui-core.js",
+    {:source-paths ["src/ui" "src/dev"]
+     :id "frontend-dev"
+     :compiler {:output-to "resources/public/js/ui-core.js"
+                :output-dir "resources/public/js/ui-out"
+                :source-map "resources/public/js/ui-core.js.map"
                 :optimizations :none
-                ; :pretty-print true
-                :source-map true
+                :cache-analysis true}}
+    {:source-paths ["src/electron"]
+     :id "electron-release"
+     :compiler {:output-to "resources/main.js"
+                :output-dir ""
+                :optimizations :simple
+                :pretty-print true
+                :cache-analysis true}}
+    {:source-paths ["src/ui" "src/dev"]
+     :id "frontend-release"
+     :compiler {:output-to "resources/public/js/ui-core.js"
+                :output-dir "resources/public/js/ui-release-out"
+                :source-map "resources/public/js/ui-core.js.map"
+                :optimizations :advanced
                 :cache-analysis true}}]}
   :figwheel {:http-server-root "public"
              :ring-handler figwheel-middleware/app
