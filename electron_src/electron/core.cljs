19dffcb17e70fb03106faafbadc9e655dff13101
Max Gonzih <gonzih@gmail.com>
2016-04-11 14:26:01 +0200

    Fix deprecation warning

--- electron_src/electron/core.cljs
+++ electron_src/electron/core.cljs
@@ -11,7 +11,7 @@
                         (clj->js {:width 800
                                   :height 600})))
   ; Path is relative to the compiled js file (main.js in our case)
-  (.loadUrl @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.loadURL @main-window (str "file://" js/__dirname "/public/index.html"))
   (.on @main-window "closed" #(reset! main-window nil)))
 
 (.start crash-reporter)
