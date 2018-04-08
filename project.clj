a0e72b0355f579e32465dcdf2d64cede7a5c198e
Alexandr G <alexandr@gavrisco.com>
2018-04-08 16:27:24 +0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
             :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
   :description "A hello world application for electron"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.542"]
-                 [figwheel "0.5.10"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [figwheel "0.5.15"]
                  [reagent "0.6.1"]
-                 [ring/ring-core "1.6.1"]]
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.10"]
+                 [ring/ring-core "1.6.3"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.15"]
             [lein-cooper "1.2.2"]]
 
   :clean-targets ^{:protect false} ["resources/main.js"
