9ea2eea4ba08a61fdc2c6ad7fd74dd77d256ceda
Max Gonzih <gonzih@gmail.com>
2019-05-30 20:33:46 -0400

    Trying to fix issue with require missing in electron ver 5+
    Relates to #20

--- electron_src/electron/core.cljs
+++ electron_src/electron/core.cljs
@@ -10,7 +10,8 @@
 (defn init-browser []
   (reset! main-window (browser-window.
                         (clj->js {:width 800
-                                  :height 600})))
+                                  :height 600
+                                  :webPreferences {:nodeIntegration true}})))
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
   (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
