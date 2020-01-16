0c6c8424ff6335e4435a569cd4218b12f679d1d2
Max Gonzih <gonzih@gmail.com>
2020-01-16 17:19:13 -0500

    Rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-electron "0.1.0-SNAPSHOT"
+(defproject clj-electron "0.1.0-SNAPSHOT"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
